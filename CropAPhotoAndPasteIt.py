ad5945a7c1d8d23b4e971a8bdea8e03cb7ea2cac
Parth Jain <159113647+Parth-2024@users.noreply.github.com>
2024-06-12 18:55:47 +0530

    Update CropAPhotoAndPasteIt.py

--- CropAPhotoAndPasteIt.py
+++ CropAPhotoAndPasteIt.py
@@ -1,9 +1,18 @@
 import cv2
-photo=cv2.imread("Nayan.png")
-print(photo.shape)
-crpPhoto=photo[100:300,250:420]
-photo[0:200,0:170]=crpPhoto
-cv2.imwrite("Crp.png",photo)
-cv2.imshow("Parth",photo)
-cv2.waitKey()
-cv2.destroyAllWindows()
+cap=cv2.VideoCapture(2)
+status,photo=cap.read()
+face_model=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+crp=face_model.detectMultiScale(photo)
+if len(crp)==0:
+    print("No Face Detected")
+else:
+    x1=crp[0][0]
+    y1=crp[0][1]
+    x2=x1+crp[0][2]
+    y2=y1+crp[0][3]
+    crop=photo[y1:y2,x1:x2]
+    photo[0:crp[0][3],0:crp[0][2]]=crop
+    cv2.imwrite("Crp.png",photo)
+    cv2.imshow("Photo in Photo",photo)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
